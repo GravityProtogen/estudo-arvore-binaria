9175199cdb620c737f189f75ea2a5084a3f4ae64
Gravity Proto <pedroarturcosta@hotmail.com>
2025-05-02 12:00:50 -0300

    Inserção.py Criado

---
+++ insercao.py
@@ -0,0 +1,54 @@
+from collections import deque
+class No:
+    def __init__(self, valor):
+        self.left=None
+        self.right=None
+        self.val=valor
+
+def inserir(root,chave):
+    if root is None:
+        return No(chave)
+    
+    queue = deque([root])
+    
+    while queue:
+        temp = queue.popleft()
+        
+        if temp.left is None:
+            temp.left = No(chave)
+            break
+        else:
+            queue.append(temp.left)
+        
+        if temp.right is None:
+            temp.right = No(chave)
+            break
+        else:
+            queue.append(temp.right)
+                    
+    return root
+
+#DFS EM ORDEM
+def dfs_em_ordem(no):
+    if no is None:
+        return
+    dfs_em_ordem(no.left)
+    print(no.val, end=' ')
+    dfs_em_ordem(no.right)
+    
+if __name__ == "__main__":
+    raiz = No(2)
+    raiz.left = No(3)
+    raiz.right = No(4)
+    raiz.left.left = No(5)
+    
+    print("Em Ordem antes de inserir ", end="")
+    dfs_em_ordem(raiz)
+    print()
+    
+    chave = 6
+    raiz = inserir(raiz, chave)
+    
+    print("Em Ordem depois de inserir ", end="")
+    dfs_em_ordem(raiz)
+    print()
